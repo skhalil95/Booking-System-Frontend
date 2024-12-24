326e401aaecaa4dd765a35cf613312c745fca725
Sara Khalil <2606122K@student.gla.ac.uk>
2024-12-24 23:31:59 +0200

    fixing layout

--- booking_system/src/App.vue
+++ booking_system/src/App.vue
@@ -7,9 +7,16 @@ import MainContent from './components/MainContent.vue';
 <template>
   <div class="flex">
     <Sidebar />
-    <div class="flex-1 flex flex-col bg-gray-100">
+    <div class="flex-1 flex flex-col bg-gray-100 transition-all duration-300 main-content">
       <TopBar />
       <MainContent />
     </div>
   </div>
 </template>
+
+<style>
+.main-content {
+  overflow-x: auto;
+}
+
+</style>
