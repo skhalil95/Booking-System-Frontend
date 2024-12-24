49b30ba8b693b8bf2c70bbd18a915c5c715ed66f
Sara Khalil <2606122K@student.gla.ac.uk>
2024-12-24 16:33:19 +0200

    time indecator

--- booking_system/src/components/CalendarView.vue
+++ booking_system/src/components/CalendarView.vue
@@ -39,7 +39,7 @@
         :interval-minutes="60"
         :weekdays="[0,1,2,3,4]"
         :disabled-before="disabledBefore"
-        :interval-count="7"
+        :interval-count="12"
         :interval-start="9"
         :interval-height="60"
         time-clicks-clamped
@@ -54,7 +54,20 @@
         @click-head-intervals="onClickHeadIntervals"
         @click-head-day="onClickHeadDay"
         class="rounded border border-gray-200 shadow-sm"
-      />
+      >
+      <template #day-container="{ scope: { days }}">
+            <template v-if="hasDate(days)">
+              <div
+                class="day-view-current-time-indicator"
+                :style="style"
+              ></div>
+              <div
+                class="day-view-current-time-line"
+                :style="style"
+              ></div>
+            </template>
+          </template>
+    </q-calendar-day>
     </div>
   </div>
 </template>
@@ -66,7 +79,8 @@ import {
   copyTimestamp,
   getDateTime,
   parseTimestamp,
-  addToDate
+  addToDate,
+  parseDate
 } from "@quasar/quasar-ui-qcalendar/src/index.js";
 import "@quasar/quasar-ui-qcalendar/src/QCalendarVariables.sass";
 import "@quasar/quasar-ui-qcalendar/src/QCalendarTransitions.sass";
@@ -87,6 +101,12 @@ export default defineComponent({
       ts = addToDate(ts, { day: -1 })
       return ts.date
     },
+    style() {
+      return {
+        top: this.timeStartPos + 'px'
+      }
+    }
+
   },
   data() {
     return {
@@ -94,9 +114,24 @@ export default defineComponent({
       selectedDates: [],
       isDialogOpen: false,
       selectedBookingTime: "",
+      currentDate: null,
+      currentTime: null,
+      timeStartPos: 0,
+      intervalId: null
     };
   },
   methods: {
+    hasDate (days) {
+      return this.currentDate
+        ? days.find(day => day.date === this.currentDate)
+        : false
+    },
+    adjustCurrentTime () {
+      const now = parseDate(new Date())
+      this.currentDate = now.date
+      this.currentTime = now.time
+      this.timeStartPos = this.$refs.calendar.timeStartPos(this.currentTime, false)
+    },
     onToggleTime({ scope }) {
       if (scope === undefined) {
         return;
@@ -156,6 +191,16 @@ export default defineComponent({
       console.log("onClickHeadDay", data);
     },
   },
+  mounted() {
+    this.adjustCurrentTime();
+      // now, adjust the time every minute
+      this.intervalId = setInterval(() => {
+        this.adjustCurrentTime()
+      }, 60000)
+  },
+  beforeUnmount() {
+    clearInterval(this.intervalId);
+  }
 });
 </script>
 
@@ -212,4 +257,34 @@ export default defineComponent({
   background-color: #d1e7dd !important;
   color: #155724 !important;
 }
+
+.day-view-current-time-indicator {
+  position: absolute;
+  left: -5px;
+  height: 10px;
+  width: 10px;
+  margin-top: -4px;
+  background-color: rgba(0, 0, 255, 0.5);
+  border-radius: 50%;
+}
+
+.day-view-current-time-line {
+  position: absolute;
+  left: 5px;
+  border-top: 2px solid rgba(0, 0, 255, 0.5);
+  width: calc(100% - 5px);
+}
+
+.q-dark .day-view-current-time-indicator,
+.body--dark .day-view-current-time-indicator,
+.q-calendar--dark .day-view-current-time-indicator {
+  background-color: rgba(255, 255, 0, 0.85);
+}
+
+.q-dark .day-view-current-time-line,
+.body--dark .day-view-current-time-line,
+.q-calendar--dark .day-view-current-time-line {
+  border-top: 2px solid rgba(255, 255, 0, 0.85);
+}
+
 </style>
